74c6509ed89a3d65bfe5d57488390c0d416faaf1
Kristian Zarębski <krizar312@gmail.com>
2024-05-19 17:36:09 +0100

    Updated for v1.10.1.1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
 ﻿
 $ErrorActionPreference = 'Stop';
-$checksum = "896e384fb70b0c3a100183f49a28133bb12fc698d116e894ce9477f7718ca684"
+$checksum = "e6bb1cac6559b3dd18641fe4954efe455d481d639f350efdbc729f21bbba52de"
 $repository = "leezer3/OpenBve"
 $software_name = "OpenBve"
 
 Write-Host "Fetching version '$env:ChocolateyPackageVersion'"
 
-$download_url = "https://github.com/$repository/releases/download/1.10.1.0/OpenBVE-1.10.1.0.zip"
+$download_url = "https://github.com/$repository/releases/download/$env:ChocolateyPackageVersion/OpenBVE-$env:ChocolateyPackageVersion.zip"
 $toolsDir = "$env:ProgramFiles\$software_name"
 
 $packageArgs = @{
